cca7fd7767d367332bc32d87fe099b6b9fcfc2d1
Vignesh <vigneswarang.20it@kongu.edu>
2022-09-13 22:41:19 +0530

    delivery boy php added

--- public/add-delivery-boy-form.php
+++ public/add-delivery-boy-form.php
@@ -5,68 +5,37 @@ $function = new functions;
 include_once('includes/custom-functions.php');
 $fn = new custom_functions;
 
-$res = $db->getResult();
 
-$sql_query = "SELECT value FROM settings WHERE variable = 'Currency'";
-$pincode_ids_exc = "";
-$db->sql($sql_query);
-$res_cur = $db->getResult();
 
 if (isset($_POST['btnAdd'])) {
-    if ($permissions['goldsmithmaster']['create'] == 1) {
-        $error = array();
-        $name = $db->escapeString($fn->xss_clean($_POST['name']));
-        $sundry = $db->escapeString($fn->xss_clean($_POST['sundry']));
-        $open_debit = $db->escapeString($fn->xss_clean($_POST['open_debit']));
-        $open_credit = $db->escapeString($fn->xss_clean($_POST['open_credit']));
-        $value =$db->escapeString($fn->xss_clean($_POST['value']));
-        $place = $db->escapeString($fn->xss_clean($_POST['place']));
-        $address = $db->escapeString($fn->xss_clean($_POST['address']));
-        $phone = $db->escapeString($fn->xss_clean($_POST['phone']));
-        $tngst = $db->escapeString($fn->xss_clean($_POST['tngst']));
-        $pure_debit=$db->escapeString($fn->xss_clean($_POST['pure_debit']));
-        $pure_credit=$db->escapeString($fn->xss_clean($_POST['pure_credit']));
+    $error = array();
+    $name = $db->escapeString($fn->xss_clean($_POST['name']));
+    $mobile = $db->escapeString($fn->xss_clean($_POST['mobile']));
+    $dob = $db->escapeString($fn->xss_clean($_POST['dob']));
+    $password = $db->escapeString($fn->xss_clean($_POST['password']));
+    $pincode = $db->escapeString($fn->xss_clean($_POST['pincode']));
+
+    $sql = "INSERT INTO deliveryboys (name,mobile,dob,password,pincode) VALUES('$name','$mobile','$dob','$password','$pincode')";
+    $db->sql($sql);
+    $deliveryboys_result = $db->getResult();
+
+    if (!empty($deliveryboys_result)) {
+        $error['add_menu'] = " <span class='label label-danger'>Failed</span>";
+        
+    } else {
+        $error['add_menu'] = " <span class='label label-success'>Success</span>";
+    }
+}
+
+    
+
 
         
-        if (empty($name)) {
-            $error['name'] = " <span class='label label-danger'>Required!</span>";
-        }
-       
-
-        if ( !empty($name))
-        {
-                $sql = "INSERT INTO goldsmith_master (name,sundry,open_debit,open_credit,value,place,address,phone,tngst,pure_debit,pure_credit) VALUES('$name','$sundry','$open_debit','$open_credit','$value','$place','$address','$phone','$tngst','$pure_debit','$pure_credit')";
-                $db->sql($sql);
-                $goldsmithmaster_result = $db->getResult();
-                if (!empty($goldsmithmaster_result)) {
-                    $goldsmithmaster_result = 0;
-                } else {
-                    $goldsmithmaster_result = 1;
-                }
-                if ($goldsmithmaster_result == 1) {
-                    $error['add_menu'] = "<section class='content-header'>
-                                                    <span class='label label-success'>Dealer Goldsmith Master Added Successfully</span>
-                                                    <h4><small><a  href='goldsmithmasters.php'><i class='fa fa-angle-double-left'></i>&nbsp;&nbsp;&nbsp;Back to Goldsmith Master</a></small></h4>
-                                                     </section>";
-                } else {
-                    $error['add_menu'] = " <span class='label label-danger'>Failed</span>";
-                }
-
-            }else{
-                $error['add_menu'] = " <span class='label label-danger'>Dealer Goldsmith Master Already Exists</span>";
-
-            }
-
-
-        }
-    }else{
-        $error['check_permission'] = " <section class='content-header'><span class='label label-danger'>You have no permission to create gold smith master</span></section>";
-
-    }
+        
 
 ?>
 <section class="content-header">
-    <h1>Add Dealer Goldsmith Master</h1>
+    <h1>Add Delivery boy</h1>
     <?php echo isset($error['add_menu']) ? $error['add_menu'] : ''; ?>
     <ol class="breadcrumb">
         <li><a href="home.php"><i class="fa fa-home"></i> Home</a></li>
@@ -93,86 +62,32 @@ if (isset($_POST['btnAdd'])) {
                                     <label for="exampleInputEmail1"> Name</label> <i class="text-danger asterik">*</i><?php echo isset($error['name']) ? $error['name'] : ''; ?>
                                     <input type="text" class="form-control" name="name" required>
                                 </div>
-                                <!-- <div class='col-md-3'>
-                                    <label for="">Sundry</label> 
-                                    <select id="sundry" name="sundry" class="form-control">
-                                        <option value="Sundry Creditors">Sundry Creditors</option>
-                                        <option value="Sundry Debitors">Sundry Debitors</option>
-                                    </select>
-                                </div> -->
+                                
                                 <div class='col-md-3'>
                                     <label for="exampleInputEmail1">Mobile</label> 
-                                    <input type="number" class="form-control" name="open_debit" >
+                                    <input type="number" class="form-control" name="mobile" >
                                 </div>
                                 <div class='col-md-3'>
                                     <label for="exampleInputEmail1">Date Of Birth</label> 
-                                    <input type="text" class="form-control" name="open_credit" >
+                                    <input type="text" class="form-control" name="dob" >
                                 </div>
                                 <div class='col-md-3'>
                                     <label for="exampleInputEmail1">Password</label> 
-                                    <input type="password" class="form-control" name="open_credit" >
+                                    <input type="password" class="form-control" name="password" >
                                 </div>
                                 <div class='col-md-3'>
                                     <label for="exampleInputEmail1">Pincode</label> 
-                                    <input type="text" class="form-control" name="open_credit" >
+                                    <input type="text" class="form-control" name="pincode" >
                                 </div>
                             </div>
 
                         </div>
-                        <!-- <hr>
-                        <div class="row">
-                            <div class="form-group">
-                            <div class='col-md-4'>
-                                    <label for="exampleInputEmail1">Value</label> 
-                                    <input type="number" class="form-control" name="value" >
-                                </div>
-                                <div class='col-md-4'>
-                                    <label for="exampleInputEmail1">Place</label> 
-                                    <input type="text" class="form-control" name="place" >
-                                </div>
-                                <div class='col-md-4'>
-                                    <label for="exampleInputEmail1">Address</label> 
-                                    <input type="text" class="form-control" name="address" >
-                                </div>
-                            </div>
-
-                        </div>
-                        <hr>
-                        <div class="row">
-                            <div class="form-group">
-                                <div class='col-md-4'>
-                                    <label for="exampleInputEmail1">Phone Number</label> 
-                                    <input type="number" class="form-control" name="phone" >
-                                </div>
-                                <div class='col-md-4'>
-                                    <label for="exampleInputEmail1">Tngst</label> 
-                                    <input type="number" class="form-control" name="tngst" >
-                                </div>
-                                
-                            </div>
-                        </div>
-                        <hr>
-                        <div class="row">
-                            <div class="form-group">
-                                <div class='col-md-4'>
-                                    <label for="exampleInputEmail1">Pure Debit</label> 
-                                    <input type="number" class="form-control" name="pure_debit" >
-                                </div>
-                                <div class='col-md-4'>
-                                    <label for="exampleInputEmail1">Pure Credit</label> 
-                                    <input type="number" class="form-control" name="pure_credit" >
-                                </div>
-                            </div>
-                                
-                        </div>
-                        <hr>
-                        </div>
-                    <!- /.box-body -->
+                        
                     <div class="box-footer">
-                        <input type="submit" class="btn-primary btn" value="Add Goldsmith Master" name="btnAdd" />&nbsp;
+                        <input type="submit" class="btn-primary btn" value="Add delivery boy" name="btnAdd" />&nbsp;
                         <!-- <input type="reset" class="btn-danger btn" value="Clear" id="btnClear" /> -->
                         <!--<div  id="res"></div>-->
-                    </div> -->
+                    </div> 
                 </form>
             </div>
 
