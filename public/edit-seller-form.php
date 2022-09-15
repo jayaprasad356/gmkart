fae8db21d7aa9323bcfe74e3f5384eba2a9dd9d4
rashmi <rashmi.kumari.it.20@ggits.net>
2022-09-15 22:38:17 +0530

    seller updated

---
+++ public/edit-seller-form.php
@@ -0,0 +1,101 @@
+<?php
+include_once('includes/functions.php');
+date_default_timezone_set('Asia/Kolkata');
+$function = new functions;
+include_once('includes/custom-functions.php');
+$fn = new custom_functions;
+
+if (isset($_GET['id'])) {
+    $ID = $db->escapeString($fn->xss_clean($_GET['id']));
+} else {
+    // $ID = "";
+    return false;
+    exit(0);
+}
+
+if (isset($_POST['btnUpdate'])) {
+    $error = array();
+    $name = $db->escapeString($fn->xss_clean($_POST['name']));
+    $email = $db->escapeString($fn->xss_clean($_POST['email']));
+    $mobile = $db->escapeString($fn->xss_clean($_POST['mobile']));
+    $pincode= $db->escapeString($fn->xss_clean($_POST['pincode']));
+    $sql = "UPDATE sellers SET name='$name', email='$email', mobile='$mobile', pincode='$pincode' WHERE id = '$ID'";
+    $db->sql($sql);
+    $sellers_result = $db->getResult();
+    if (!empty($sellers_result)) {
+        $error['add_menu'] = " <span class='label label-danger'>Failed</span>";
+    } else {
+        $error['add_menu'] = " <span class='label label-success'>Success</span>";
+    }
+    
+}
+
+$data = array();
+
+$sql_query = "SELECT * FROM `sellers` WHERE id = '$ID'";
+$db->sql($sql_query);
+$res = $db->getResult();
+foreach ($res as $row)
+$data = $row;
+
+?>
+<section class="content-header">
+    <h1>Edit Dealer Goldsmith Master</h1>
+    <?php echo isset($error['add_menu']) ? $error['add_menu'] : ''; ?>
+    <ol class="breadcrumb">
+        <li><a href="home.php"><i class="fa fa-home"></i> Home</a></li>
+    </ol>
+
+</section>
+<section class="content">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box box-primary">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Edit Dealer Goldsmith Master</h3>
+                </div>
+                <div class="box-header">
+                    <?php echo isset($error['cancelable']) ? '<span class="label label-danger">Till status is required.</span>' : ''; ?>
+                </div>
+
+                <!-- /.box-header -->
+                <!-- form start -->
+                <form id='edit_sellers_form' method="post" enctype="multipart/form-data">
+                    <div class="box-body">
+                        <div class="row">
+                            <div class="form-group">
+                                <div class='col-md-3'>
+                                    <label for="exampleInputEmail1"> Name</label> <i class="text-danger asterik">*</i><?php echo isset($error['name']) ? $error['name'] : ''; ?>
+                                    <input type="text" class="form-control" name="name" value="<?php echo $data['name']?>" required>
+                                </div>
+                                <div class='col-md-3'>
+                                    <label for="exampleInputEmail1">Email</label> <i class="text-danger asterik">*</i><?php echo isset($error['email']) ? $error['email'] : ''; ?>
+                                    <input type="text" class="form-control" name="email" value="<?php echo $data['email']?>" required>
+                                </div>
+                                <div class='col-m
+                               d-3'>
+                                    <label for="exampleInputEmail1"> mobile</label> <i class="text-danger asterik">*</i><?php echo isset($error['mobile']) ? $error['mobile'] : ''; ?>
+                                    <input type="text" class="form-control" name="mobile" value="<?php echo $data['mobile']?>" required>
+                                </div>
+                                <div class='col-md-3'>
+                                    <label for="exampleInputEmail1">pincode</label> <i class="text-danger asterik">*</i><?php echo isset($error['pincode']) ? $error['pincode'] : ''; ?>
+                                    <input type="text" class="form-control" name="pincode" value="<?php echo $data['pincode']?>" required>
+                                </div>
+                        <hr>
+
+                </div><!-- /.box-body -->
+                    
+                    <div class="box-footer">
+                        <input type="submit" class="btn-primary btn" value="Update" name="btnUpdate" />&nbsp;
+                        <!-- <input type="reset" class="btn-danger btn" value="Clear" id="btnClear" /> -->
+                        <!--<div  id="res"></div>-->
+                    </div>
+                </form>
+            </div>
+            <!-- /.box -->
+        </div>
+    </div>
+</section>
+<div class="separator"> </div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
